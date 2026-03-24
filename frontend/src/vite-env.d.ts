a9c8feaceb119f9a59336d79e3a41ca6cd89e7a4
Rkiran40 <rkirangh07@gmail.com>
2026-03-24 14:09:29 +0530

    Fixed Problems

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly VITE_API_BASE_URL: string;
-	// add other env variables here as needed
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
-
